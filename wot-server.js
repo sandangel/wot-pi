c416dd661d9cf6933f60e59c55449bcac524e13f
vinhsannguyen91 <san.nguyen@tanaka-scale.co.jp>
2016-10-28 15:28:27 +0900

    wot code

--- wot-server.js
+++ wot-server.js
@@ -1,11 +1,6 @@
-#!/usr/bin/nodejs
-
-var debug = require('debug')('my-application');
 var httpServer = require('./servers/http');
 var resources = require('./resources/model');
 
-httpServer.set('port', resources.pi.port);
-
-var server = httpServer.listen(httpServer.get('port'), function () {
-  console.info('Express server listening on port ' + server.address().port);
+var server = httpServer.listen(resources.pi.port, function () {
+  console.info('Express server listening on port %s', resources.pi.port);
 });
