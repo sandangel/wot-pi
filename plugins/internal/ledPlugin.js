bd15a4417f68baedbc0fcfcc0e543a84ddfdd616
vinhsannguyen91 <san.nguyen@tanaka-scale.co.jp>
2016-10-31 14:54:38 +0900

    led

--- plugins/internal/ledPlugin.js
+++ plugins/internal/ledPlugin.js
@@ -22,7 +22,7 @@ exports.stop = function () {
 function observe(what) {
   Object.observe(what, function (changes) {
     console.info('Change detected by plugin for %s...', pluginName);
-    switchOnOff(model.value);
+    switchOnOff(model.value); //#B
   });
 };
 
