c0892259f6d83eb7e50af3bdbcc07ee7ea67c904
vinhsannguyen91 <san.nguyen@tanaka-scale.co.jp>
2016-10-31 16:26:19 +0900

    led

--- wot-server.js
+++ wot-server.js
@@ -4,18 +4,18 @@ var ledsPlugin = require('./plugins/internal/ledPlugin'),
   pirPlugin = require('./plugins/internal/pirPlugin'),
   dhtPlugin = require('./plugins/internal/DHT11SensorPlugin');
 
-// pirPlugin.start({
-//   'simulate': false,
-//   'frequency': 2000
-// });
-// dhtPlugin.start({
-//   'simulate': false,
-//   'frequency': 10000
-// });
-ledsPlugin.start({
+pirPlugin.start({
+  'simulate': false,
+  'frequency': 2000
+});
+dhtPlugin.start({
   'simulate': false,
   'frequency': 10000
 });
+// ledsPlugin.start({
+//   'simulate': false,
+//   'frequency': 10000
+// });
 
 var server = httpServer.listen(resources.pi.port, function () {
   console.info('Express server listening on port %s', resources.pi.port);
