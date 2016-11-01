4a59b52db3e0062a59d767abd3dd6e059c55c97c
vinhsannguyen91 <san.nguyen@tanaka-scale.co.jp>
2016-11-01 11:41:45 +0900

    Use Proxy instead of O.o

--- plugins/internal/ledPlugin.js
+++ plugins/internal/ledPlugin.js
@@ -6,7 +6,11 @@ var pluginName = model.name;
 
 exports.start = function () {
 
-  var objectToObserve = new Proxy(model, objectChangeHandler)
+  // var objectToObserve = new Proxy(model, objectChangeHandler)
+  create_gets_sets(model);
+  listen_to(model, "value", function (oldval, newval) {
+    console.info("old : " + oldval + " new : " + newval);
+  });
   connectHardware();
   // var proxied = new Proxy(model, {
   //   get: function (target, prop) {
@@ -20,23 +24,51 @@ exports.start = function () {
 exports.stop = function () {
   actuator.unexport();
   console.info('%s plugin stopped!', pluginName);
-}
-
-var objectChangeHandler = {
-  apply: function (target, thisArg, argumentsList) {
-    return thisArg[target].apply(this, argumentList);
-  },
-  deleteProperty: function (target, property) {
-    console.log("Deleted %s", property);
-    return true;
-  },
-  set: function (target, property, value, receiver) {
-    target[property] = value;
-    switchOnOff(value);
-    console.log("Set %s to %o", property, value);
-    return true;
-  }
 };
+
+function create_gets_sets(obj) {
+  var proxy = {};
+  for (var i in obj) {
+    if (obj.hasOwnProperty(i)) {
+      var k = i;
+      proxy["set_" + i] = function (val) {
+        this[k] = val;
+      };
+      proxy["get_" + i] = function () {
+        return this[k];
+      };
+    }
+  };
+  for (var i in proxy) {
+    if (proxy.hasOwnProperty(i)) {
+      obj[i] = proxy[i];
+    }
+  };
+};
+
+function listen_to(obj, prop, handler) {
+  var current_setter = obj["set_" + prop];
+  var old_val = obj["get_" + prop]();
+  obj["set_" + prop] = function (val) {
+    current_setter.apply(obj, [old_val, val]);
+    handler(val);
+  };
+};
+// var objectChangeHandler = {
+//   apply: function (target, thisArg, argumentsList) {
+//     return thisArg[target].apply(this, argumentList);
+//   },
+//   deleteProperty: function (target, property) {
+//     console.log("Deleted %s", property);
+//     return true;
+//   },
+//   set: function (target, property, value, receiver) {
+//     target[property] = value;
+//     switchOnOff(value);
+//     console.log("Set %s to %o", property, value);
+//     return true;
+//   }
+// };
 // function observe(what) {
 //   Object.observe(what, function (changes) {
 //     console.info('Change detected by plugin for %s...', pluginName);
