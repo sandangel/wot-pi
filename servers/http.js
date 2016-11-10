35e12f2ee140dfb62328637a49dd11fd6ef7319a
sandangel <vinhsannguyen91@gmail.com>
2016-11-10 13:58:31 +0900

    add route for /things

--- servers/http.js
+++ servers/http.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var actuatorsRoutes = require('./../routes/actuators');
 var sensorsRoutes = require('./../routes/sensors');
+var thingsRoutes = require('./../routes/things');
 var resources = require('./../resources/model');
 var converter = require('./../middleware/converter');
 var cors = require('cors');
@@ -8,14 +9,18 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(bodyParser.json());
+// app.set('view engine', 'ejs');
+// app.set('views', __dirname.replace('servers', 'views'));
 
+app.use(bodyParser.json());
+app.use(express.static(__dirname.replace('servers', 'public')));
 app.use(cors());
 
 app.use('/pi/actuators', actuatorsRoutes);
 app.use('/pi/sensors', sensorsRoutes);
+app.use('/things', thingsRoutes);
 
-app.get('/pi', function (req, res) {
+app.get('/pi', function(req, res) {
   res.send('This is the WoT-Pi');
 });
 
