07a8c6fd615dd10fef9b64ea0b2b8a39c49fa116
sandangel <vinhsannguyen91@gmail.com>
2016-11-15 18:40:42 +0900

    split file

--- app.js
+++ app.js
@@ -6,43 +6,43 @@ var ledsPlugin = require('./plugins/ledPlugin')
 
 var bodyParser = require('body-parser');
 
-var brightness = 0;
-var obj;
-fs.readFile('./resources/resources.json', 'utf8', function(err, data) {
-  if (err) throw err;
-  obj = JSON.parse(data);
-});
+// var brightness = 0;
+// var obj;
+// fs.readFile('./resources/resources.json', 'utf8', function(err, data) {
+//   if (err) throw err;
+//   obj = JSON.parse(data);
+// });
 // fs.writeFileSync('./resources/resources-update.json', JSON.stringify(obj));
-var resources = require('./resources/resources.json');
-var actuator;
+var resources = require('./resources/model');
+// var actuator;
 var model = resources.pi.actuators.leds['1'];
-var pluginName = model.name;
-var WatchJS = require('watchjs');
-
-var watch = WatchJS.watch;
-var unwatch = WatchJS.unwatch;
-var calWatchers = WatchJS.calWatchers;
-
-function switchOnOff(value) {
-  actuator.write(value === true ? 1 : 0, function() {
-    console.info('Changed value of %s to %s', pluginName, value);
-  });
-};
-
-function connectHardware() {
-  var Gpio = require('onoff').Gpio;
-  actuator = new Gpio(model.gpio, 'out');
-  console.info('Hardware %s actuator started!', pluginName);
-};
-
-function start() {
-  watch(model, 'value', function(prop, action, newvalue, oldvalue) {
-    console.info(prop + " - action: " + action + " - new: " + newvalue + ", old: " + oldvalue + "... and the context: " + JSON.stringify(this));
-    switchOnOff(newvalue);
-  });
-
-  connectHardware();
-};
+// var pluginName = model.name;
+// var WatchJS = require('watchjs');
+//
+// var watch = WatchJS.watch;
+// var unwatch = WatchJS.unwatch;
+// var calWatchers = WatchJS.calWatchers;
+//
+// function switchOnOff(value) {
+//   actuator.write(value === true ? 1 : 0, function() {
+//     console.info('Changed value of %s to %s', pluginName, value);
+//   });
+// };
+//
+// function connectHardware() {
+//   var Gpio = require('onoff').Gpio;
+//   actuator = new Gpio(model.gpio, 'out');
+//   console.info('Hardware %s actuator started!', pluginName);
+// };
+//
+// function start() {
+//   watch(model, 'value', function(prop, action, newvalue, oldvalue) {
+//     console.info(prop + " - action: " + action + " - new: " + newvalue + ", old: " + oldvalue + "... and the context: " + JSON.stringify(this));
+//     switchOnOff(newvalue);
+//   });
+//
+//   connectHardware();
+// };
 
 
 app.use(bodyParser.json());
@@ -61,9 +61,7 @@ io.on('connection', function(socket) {
   });
 })
 
-
-// ledsPlugin.start();
-start();
+ledsPlugin.start();
 
 http.listen(3000, function() {
   console.log('listening on *:3000');
--- resources/model.js
+++ resources/model.js
@@ -1,3 +1,3 @@
-var resources = require('./resources-update.json');
+var resources = require('./resources.json');
 
 module.exports = resources;
