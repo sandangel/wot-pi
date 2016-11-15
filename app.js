25cac5bed233694be45981ba337fa4dd62bca0f7
sandangel <vinhsannguyen91@gmail.com>
2016-11-15 17:09:48 +0900

    merge plugin in app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var io = require('socket.io')(http);
 var fs = require('fs');
 var ledsPlugin = require('./plugins/ledPlugin')
 
-var resources = require('./resources/model');
 var bodyParser = require('body-parser');
 
 var brightness = 0;
@@ -15,35 +14,44 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/leds/:id', function(req, res, next) {
-  req.result = resources.pi.actuators.leds[req.params.id];
-  if (req.result) {
-    if (req.accepts('json')) {
-      console.info('JSON representation selected!');
-      res.send(req.result);
-    }
-  }
-  next();
-});
-
 io.on('connection', function(socket) {
   console.log('user connected');
   socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
     if (msg.toString() == "true") {
       var obj;
-      console.log('reading');
       fs.readFile('./resources/resources.json', 'utf8', function(err, data) {
         if (err) throw err;
         obj = JSON.parse(data);
         obj.pi.actuators.leds[1].value = msg;
         fs.writeFile('./resources/resources.json', JSON.stringify(obj));
+        var resources = require('./resources/model');
+        var actuator;
+        var model = resources.pi.actuators.leds['1'];
+        var pluginName = model.name;
+
+        function switchOnOff(value) {
+          actuator.write(value === true ? 1 : 0, function() {
+            console.info('Changed value of %s to %s', pluginName, value);
+          });
+        };
+
+        function connectHardware() {
+          var Gpio = require('onoff').Gpio;
+          actuator = new Gpio(model.gpio, 'out');
+          console.info('Hardware %s actuator started!', pluginName);
+        };
+
+        connectHardware();
+        switchOnOff(resources.pi.actuators.leds[1].value);
+        console.log(resources.pi.actuators.leds[1].value);
       });
     }
   });
-});
+})
 
-ledsPlugin.start();
+
+// ledsPlugin.start();
 
 http.listen(3000, function() {
   console.log('listening on *:3000');
