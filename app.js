04e2531a8957a4e1c357bab6d059dd3633fc774f
sandangel <vinhsannguyen91@gmail.com>
2016-11-15 15:59:05 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+var ledsPlugin = require('./plugins/ledPlugin')
+
+var resources = require('./resources/model');
+var bodyParser = require('body-parser');
+
+var brightness = 0;
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/leds/:id', function(req, res, next) {
+  req.result = resources.pi.actuators.leds[req.params.id];
+  if (req.result) {
+    if (req.accepts('json')) {
+      console.info('JSON representation selected!');
+      res.send(req.result);
+    }
+  }
+  next();
+});
+
+io.on('connection', function(socket) {
+  console.log('user connected');
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+    if (msg.toString() == "true") {
+      var obj;
+      console.log('reading');
+      fs.readFile('./resources/resources.json', 'utf8', function(err, data) {
+        if (err) throw err;
+        obj = JSON.parse(data);
+        obj.pi.actuators.leds[1].value = msg;
+        fs.writeFile('./resources/test.json', JSON.stringify(obj));
+      });
+    }
+  });
+});
+
+ledPlugin.start();
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
