3986eda6365dd05f597185289b0755ae940453c6
sandangel <vinhsannguyen91@gmail.com>
2016-11-15 18:05:49 +0900

    use obj

--- app.js
+++ app.js
@@ -7,6 +7,43 @@ var ledsPlugin = require('./plugins/ledPlugin')
 var bodyParser = require('body-parser');
 
 var brightness = 0;
+var obj;
+fs.readFile('./resources/resources.json', 'utf8', function(err, data) {
+  if (err) throw err;
+  obj = JSON.parse(data);
+});
+// fs.writeFileSync('./resources/resources-update.json', JSON.stringify(obj));
+var resources = obj;
+var actuator;
+var model = resources.pi.actuators.leds['1'];
+var pluginName = model.name;
+var WatchJS = require('watchjs');
+
+var watch = WatchJS.watch;
+var unwatch = WatchJS.unwatch;
+var calWatchers = WatchJS.calWatchers;
+
+function switchOnOff(value) {
+  actuator.write(value === true ? 1 : 0, function() {
+    console.info('Changed value of %s to %s', pluginName, value);
+  });
+};
+
+function connectHardware() {
+  var Gpio = require('onoff').Gpio;
+  actuator = new Gpio(model.gpio, 'out');
+  console.info('Hardware %s actuator started!', pluginName);
+};
+
+function start() {
+  watch(model, 'value', function(prop, action, newvalue, oldvalue) {
+    console.info(prop + " - action: " + action + " - new: " + newvalue + ", old: " + oldvalue + "... and the context: " + JSON.stringify(this));
+    switchOnOff(newvalue);
+  });
+
+  connectHardware();
+};
+
 
 app.use(bodyParser.json());
 
@@ -19,39 +56,14 @@ io.on('connection', function(socket) {
   socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
     if (msg.toString() == "true") {
-      var obj;
-      fs.readFile('./resources/resources-update.json', 'utf8', function(err, data) {
-        if (err) throw err;
-        obj = JSON.parse(data);
-        obj.pi.actuators.leds[1].value = msg;
-        fs.writeFileSync('./resources/resources-update.json', JSON.stringify(obj));
-        var resources = require('./resources/model');
-        var actuator;
-        var model = resources.pi.actuators.leds['1'];
-        var pluginName = model.name;
-
-        function switchOnOff(value) {
-          actuator.write(value === true ? 1 : 0, function() {
-            console.info('Changed value of %s to %s', pluginName, value);
-          });
-        };
-
-        function connectHardware() {
-          var Gpio = require('onoff').Gpio;
-          actuator = new Gpio(model.gpio, 'out');
-          console.info('Hardware %s actuator started!', pluginName);
-        };
-
-        connectHardware();
-        switchOnOff(resources.pi.actuators.leds[1].value);
-        console.log(resources.pi.actuators.leds[1].value);
-      });
+      model.pi.actuators.leds['1'].value = "true";
     }
   });
 })
 
 
 // ledsPlugin.start();
+start();
 
 http.listen(3000, function() {
   console.log('listening on *:3000');
