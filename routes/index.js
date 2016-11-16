f98ad0dc8e5ba88803e720480221bd7b27943ca8
sandangel <vinhsannguyen91@gmail.com>
2016-11-16 16:17:44 +0900

    restart plug

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var ledsPlugin = require('../plugins/ledPlugin');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   ledsPlugin.stop();
+  ledsPlugin.start();
   res.render('index', {
     "led1": "off",
     "led2": "off"
