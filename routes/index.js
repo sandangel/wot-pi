45f621927537fe116ec87b9a91eb8c4d06038991
sandangel <vinhsannguyen91@gmail.com>
2016-11-16 16:15:22 +0900

    refresh page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var ledsPlugin = require('../plugins/ledPlugin');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  ledsPlugin.stop();
+  res.render('index', {
+    "led1": "off",
+    "led2": "off"
+  });
 });
 
 module.exports = router;
