e5a2ffa16c996081780d2688739d75e80878327f
sandangel <vinhsannguyen91@gmail.com>
2016-11-16 16:30:34 +0900

    read led status

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var ledsPlugin = require('../plugins/ledPlugin');
-
+var resources = require('../resources/model');
+var model = [];
+model.push(resources.pi.actuators.leds['1']);
+model.push(resources.pi.actuators.leds['2']);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  ledsPlugin.stop();
-  ledsPlugin.start();
+  // ledsPlugin.stop();
+  // ledsPlugin.start();
   res.render('index', {
-    "led1": "off",
-    "led2": "off"
+    "led1": model[0].value,
+    "led2": model[0].value
   });
 });
 
