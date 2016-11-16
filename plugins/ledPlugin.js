450bdbe07de4bef9f90c57f6f07f029e64a68ea7
sandangel <vinhsannguyen91@gmail.com>
2016-11-16 09:27:36 +0900

    try model array

--- plugins/ledPlugin.js
+++ plugins/ledPlugin.js
@@ -6,34 +6,34 @@ var unwatch = WatchJS.unwatch;
 var calWatchers = WatchJS.calWatchers;
 
 var actuator;
-var model1 = resources.pi.actuators.leds['1'];
-var model2 = resources.pi.actuators.leds['2'];
+var model = [];
+// var model1 = resources.pi.actuators.leds['1'];
+// var model2 = resources.pi.actuators.leds['2'];
 // var pluginName = model.name;
 
 exports.start = function() {
-
-  watch(model1, 'value', function(prop, action, newvalue, oldvalue) {
-    // console.info(prop + " - action: " + action + " - new: " + newvalue + ", old: " + oldvalue + "... and the context: " + JSON.stringify(this));
+  model.push(resources.pi.actuators.leds['1']);
+  model.push(resources.pi.actuators.leds['2']);
+  watch(model[0], 'value', function(prop, action, newvalue, oldvalue) {
     switchOnOff(newvalue);
   });
 
   // watch(model2, 'value', function(prop, action, newvalue, oldvalue) {
-  //   console.info(prop + " - action: " + action + " - new: " + newvalue + ", old: " + oldvalue + "... and the context: " + JSON.stringify(this));
   //   switchOnOff(newvalue);
   // });
 
-  connectHardware(model1);
+  connectHardware(model[0]);
   // connectHardware(model2);
 };
 
 exports.stop = function() {
   actuator.unexport();
-  console.info('%s plugin stopped!', model1.name);
+  console.info('%s plugin stopped!', model[0].name);
 };
 
 function switchOnOff(value) {
   actuator.write(value === true ? 1 : 0, function() {
-    console.info('Changed value of %s to %s', model1.name, value);
+    console.info('Changed value of %s to %s', model[0].name, value);
   });
 };
 
@@ -41,5 +41,5 @@ function connectHardware(model) {
   var Gpio = require('onoff').Gpio;
   actuator = new Gpio(model.gpio, 'out');
   console.info(actuator);
-  console.info('Hardware %s actuator started!', model1.name);
+  console.info('Hardware %s actuator started!', model[0].name);
 };
